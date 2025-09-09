67190f1d233c5059b31031dd0e94ae9aa76c3f8c
Fano-Rdz <178584@upslp.edu.mx>
2025-09-09 09:08:46 -0600

    Test2

--- tests/test_database.py
+++ tests/test_database.py
@@ -13,27 +13,27 @@ def db_connection():
     yield conn
     conn.close()
 
-def test_alumno_insertados(db_connection):
+def test_alumnos_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM alumnos;")
+        cur.execute("SELECT COUNT(*) FROM alumno;")
         count = cur.fetchone()[0]
         assert count == 10
 
-def test_maestro_insertados(db_connection):
+def test_maestros_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM maestros;")
+        cur.execute("SELECT COUNT(*) FROM maestro;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_grupo_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM grupos;")
+        cur.execute("SELECT COUNT(*) FROM grupo;")
         count = cur.fetchone()[0]
         assert count == 10
 
 def test_inscripcion_insertados(db_connection):
     with db_connection.cursor() as cur:
-        cur.execute("SELECT COUNT(*) FROM inscripciones;")
+        cur.execute("SELECT COUNT(*) FROM inscripcion;")
         count = cur.fetchone()[0]
         assert count == 10  
 
@@ -51,7 +51,7 @@ def test_structure(db_connection):
             'inscripcion', 'asistencia');
           '''
     expected_tables = {'alumno', 'maestro',
-                       'grupo', 'inscripcione',
+                       'grupo', 'inscripcion',
                        'asistencia'}
     with db_connection.cursor() as cur:
         cur.execute(sql)
