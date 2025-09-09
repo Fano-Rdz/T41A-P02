e653f1f9b77a98c471aee116be5afe190a3e8021
Fano-Rdz <178584@upslp.edu.mx>
2025-09-08 19:18:07 -0600

    Feature/178584

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,44 +1,41 @@
 CREATE TABLE alumno (
-  id SERIAL PRIMARY KEY,
-  nombre VARCHAR(40) NOT NULL,
-  apellidos VARCHAR(40) NOT NULL
+  id VARCHAR(10) PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL
 );
- 
--- Maestro
+
+-- Crear tabla Maestro
 CREATE TABLE maestro (
-  id SERIAL PRIMARY KEY,
-  nombre VARCHAR(40) NOT NULL,
-  apellidos VARCHAR(40) NOT NULL
+  id VARCHAR(10) PRIMARY KEY,
+  nombre VARCHAR(40) NOT NULL
 );
- 
--- Grupo (con maestro asignado)
+
+-- Crear tabla Grupo
 CREATE TABLE grupo (
-  id SERIAL PRIMARY KEY,
-  periodo VARCHAR(20) NOT NULL,     
+  id VARCHAR(10) PRIMARY KEY,
+  periodo VARCHAR(20) NOT NULL,
+  seccion VARCHAR(20) NOT NULL,
   materia VARCHAR(20) NOT NULL,
-  maestro_id INTEGER NOT NULL,
+  maestro_id VARCHAR(10),
   FOREIGN KEY (maestro_id) REFERENCES maestro(id)
 );
- 
--- Inscripcion 
+
+-- Crear tabla Inscripcion
 CREATE TABLE inscripcion (
-  id SERIAL NOT NULL,
-  anio INTEGER NOT NULL,
-  periodo VARCHAR(20),             
-  grupo_id INTEGER NOT NULL,
-  alumno_id INTEGER NOT NULL,
-  FOREIGN KEY (grupo_id) REFERENCES grupo(id),
+  alumno_id VARCHAR(10),
+  grupo_id VARCHAR(10),
+  fecha DATE NOT NULL,
   FOREIGN KEY (alumno_id) REFERENCES alumno(id),
-  PRIMARY KEY (anio, periodo, id, alumno_id)
+  FOREIGN KEY (grupo_id) REFERENCES grupo(id),
+  PRIMARY KEY (alumno_id, grupo_id)
 );
- 
--- Asistencia
+
+-- Crear tabla Asistencia
 CREATE TABLE asistencia (
-  alumno_id INTEGER NOT NULL,
+  alumno_id VARCHAR(10) NOT NULL,
   fecha_hora TIMESTAMP NOT NULL,
   presente BOOLEAN NOT NULL,
-  grupo_id INTEGER NOT NULL,
-  PRIMARY KEY (alumno_id, fecha_hora, presente),
+  grupo_id VARCHAR(10) NOT NULL,
+  PRIMARY KEY (alumno_id, fecha_hora, grupo_id),
   FOREIGN KEY (alumno_id) REFERENCES alumno(id),
   FOREIGN KEY (grupo_id) REFERENCES grupo(id)
 );
